780acacaffa8f65a840c8fe48334c74334af0403
inline-arc <atharvmore20@gmail.com>
2025-02-15 14:41:24 +0530

    deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 'use client';
 
-import FundCardEth from "./components/FundCard";
-import NFTCardEth from "./components/NFTCard";
-import SwapCardEth from "./components/SwapCard";
+
 import Sidebar from "./components/ui/Sidebar";
-import Home from "./components/ui/Home";
+
 
 
 export default function App() {
